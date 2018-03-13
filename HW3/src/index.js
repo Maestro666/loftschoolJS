3411c84fecde9534ad6db5355b73398de44d5a3e
Andrew Fok <andrewmaestro1991@gmail.com>
2018-03-13 12:09:26 +0200

    HW3 is done

---
+++ HW3/src/index.js
@@ -0,0 +1,140 @@
+/* ДЗ 3 - работа с массивами и объеектами */
+
+/*
+ Задача 1:
+ Напишите аналог встроенного метода forEach для работы с массивами
+ */
+function forEach(array, fn) {
+    for (let i = 0; i < array.length; i++) {
+        fn(array[i], i, array);
+    }
+}
+
+/*
+ Задача 2:
+ Напишите аналог встроенного метода map для работы с массивами
+ */
+function map(array, fn) {
+    let customArr = [];
+
+    for (let i = 0; i < array.length; i++) {
+        customArr[i] = fn(array[i], i, array);
+    }
+
+    return customArr;
+}
+
+/*
+ Задача 3:
+ Напишите аналог встроенного метода reduce для работы с массивами
+ */
+function reduce(array, fn, initial) {
+    let res = 0;
+    let i = 0;
+
+    if (initial) {
+        res = initial;
+    } else {
+        res = array[0];
+        i++;
+    }
+
+    for (; i < array.length; i++) {
+        res = fn(res, array[i], i, array);
+    }
+
+    return res;
+}
+
+/*
+ Задача 4:
+ Функция принимает объект и имя свойства, которое необходиом удалить из объекта
+ Функция должна удалить указанное свойство из указанного объекта
+ */
+function deleteProperty(obj, prop) {
+    delete obj[prop];
+}
+
+/*
+ Задача 5:
+ Функция принимает объект и имя свойства и возвращает true или false
+ Функция должна проверить существует ли укзаанное свойство в указанном объекте
+ */
+function hasProperty(obj, prop) {
+    return obj.hasOwnProperty(prop)
+}
+
+/*
+ Задача 6:
+ Функция должна получить все перечисляемые свойства объекта и вернуть их в виде массива
+ */
+function getEnumProps(obj) {
+    return Object.keys(obj);
+}
+
+/*
+ Задача 7:
+ Функция должна перебрать все свойства объекта, преобразовать их имена в верхний регистра и вернуть в виде массива
+ */
+function upperProps(obj) {
+    let newArr = [];
+
+    for (let prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            newArr.push(prop.toUpperCase());
+        }
+    }
+
+    return newArr;
+}
+
+/*
+ Задача 8 *:
+ Напишите аналог встроенного метода slice для работы с массивами
+ */
+function slice(array, from, to) {
+    let result = [];
+
+    if (from === undefined || from < 0) {
+        from = 0;
+    }
+
+    if (to < 0) {
+        to += array.length;
+    } else if (to === undefined || to > array.length) {
+        to = array.length;
+    }
+
+    for (from; from < to; from++) {
+        result.push(array[from]);
+    }
+
+    return result;
+}
+
+/*
+ Задача 9 *:
+ Функция принимает объект и должна вернуть Proxy для этого объекта
+ Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
+ */
+function createProxy(obj) {
+    return new Proxy(obj, {
+        set(target, prop, value) {
+            target[prop] = Math.pow(value, 2);
+
+            return true;
+        }
+    });
+}
+
+export {
+    forEach,
+    map,
+    reduce,
+    deleteProperty,
+    hasProperty,
+    getEnumProps,
+    upperProps,
+    slice,
+    createProxy
+};
